e8f1f66f556c1555ced52bf169f46d80a9dea490
Eduardo Chaya <eduardo.chaya@devoteam.com>
2025-06-23 14:58:55 +0200

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,17 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', { required: true })
+    const bucketRegion = core.getInput('bucket-region', { required: true })
+    const distFolder = core.getInput('dist-folder', { required: true })
+
+    core.notice(`bucket: ${bucket}`)
+    // core.notice(bucketRegion)
+    // core.notice(distFolder)
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`echo "aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}"`)
+
     core.notice('Hello from my custome JavaScript Action!')
 
 }
