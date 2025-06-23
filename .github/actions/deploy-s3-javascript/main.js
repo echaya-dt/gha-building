14580c43cb8e463f12a722363d3bce4aab97e277
Eduardo Chaya <eduardo.chaya@devoteam.com>
2025-06-23 11:56:07 +0200

    added first custom js action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core')
+const core = require('@actions/github')
+const core = require('@actions/exec')
+
+function run() {
+    core.notice('Hello from my custome JavaScript Action!')
+
+}
+
+run();
